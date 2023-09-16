34a188251acd4fd19deeebab2b20311cc5c323a6
Yunjin <jed.donaire08@gmail.com>
2023-09-17 06:18:14 +0800

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Navbar from "./Navbar";
+import Billings from "./Billings";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Client from "./Client";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billings,
+  CardDeal,
+  Business,
+  Client,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
