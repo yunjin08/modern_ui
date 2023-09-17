a655a2912361e256ce9f57a0de63a65da5ca50be
Yunjin <jed.donaire08@gmail.com>
2023-09-17 13:33:53 +0800

    Added The Business Features

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-function Button() {
-  return <div>Button</div>;
+function Button({ styles }) {
+  return (
+    <button
+      type="button"
+      className={`py-4 px-6 bg-blue-gradient font-popping font-medium text-[18px] text-primary outline-none ${styles}`}
+    >
+      Get Started
+    </button>
+  );
 }
 
 export default Button;
--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { stats } from "../constants";
+import styles from "../style";
 
 const Stats = () => {
-  return <div>S</div>;
+  return (
+    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb mb-6`}>
+      {stats.map((stat) => (
+        <div
+          key={stat.id}
+          className={`flex flex-1 justify-center items-center m-3`}
+        >
+          <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+            {stat.value}
+          </h4>
+          <p
+            className={`font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] ${styles.textGradient}`}
+          >
+            {stat.title}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Stats;
