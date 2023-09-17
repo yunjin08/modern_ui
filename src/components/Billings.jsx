bcf4017b95620098967ecfb85a20c85f926e2d22
Yunjin <jed.donaire08@gmail.com>
2023-09-17 14:08:39 +0800

    Added the billing img

--- src/components/Billings.jsx
+++ src/components/Billings.jsx
@@ -1,7 +1,20 @@
-import { useState } from "react";
+import { apple, bill, google } from "../assets";
+import styles, { layout } from "../style";
 
 const Billings = () => {
-  return <div>Billings</div>;
+  return (
+    <section id="product" className={`${layout.sectionReverse}`}>
+      <div className={`${layout.sectionImgReverse}`}>
+        <img
+          src={bill}
+          alt="billing"
+          className="w-[100%] h-[100%] relative z-[5] "
+        />
+        <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
+        <div className="absolute z-[0] -left-1/2 top-0 w-[50%] h-[50%] rounded-full pink__gradient" />
+      </div>
+    </section>
+  );
 };
 
 export default Billings;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ function Button({ styles }) {
   return (
     <button
       type="button"
-      className={`py-4 px-6 bg-blue-gradient font-popping font-medium text-[18px] text-primary outline-none ${styles}`}
+      className={`py-4 px-6 bg-blue-gradient font-popping font-medium rounded-md text-[18px] text-primary outline-none ${styles}`}
     >
       Get Started
     </button>
