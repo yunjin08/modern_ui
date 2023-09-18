4d479b933ff804655145a8156646d3b857f8d288
Yunjin <jed.donaire08@gmail.com>
2023-09-19 03:42:47 +0800

    Added Card Deal

--- src/components/Billings.jsx
+++ src/components/Billings.jsx
@@ -13,6 +13,31 @@ const Billings = () => {
         <div className="absolute z-[3] -left-1/2 top-0 w-[50%] h-[50%] rounded-full white__gradient" />
         <div className="absolute z-[0] -left-1/2 top-0 w-[50%] h-[50%] rounded-full pink__gradient" />
       </div>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>
+          Easily control your <br className="sm:block hidden" />
+          billing & invoicing.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[570px] mt-5`}>
+          Hoobank: Where Invoicing and Billing Excellence Meets Financial
+          Precision. Our mission is to provide individuals and businesses with a
+          seamless platform to manage their money efficiently, offering
+          user-friendly tools for generating invoices, tracking payments, and
+          maintaining financial clarity.
+        </p>
+        <div className="flex sm:mt-10 mt-6">
+          <img
+            src={apple}
+            alt="apple_store"
+            className="object-contain w-[128px] h-[48px] mr-5 cursor-pointer"
+          />
+          <img
+            src={google}
+            alt="google_store"
+            className="object-contain w-[128px] h-[48px] cursor-pointer"
+          />
+        </div>
+      </div>
     </section>
   );
 };
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
 
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section className={`${layout.section}`}>
+      <div className={`${layout.sectionInfo}`}>
+        <h2 className={`${styles.heading2}`}>
+          {" "}
+          Find a better card deal <br className="sm:block hidden" /> in few easy
+          steps.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[480px] mt-5`}>
+          Unlock your perfect card deal in 3 easy steps! Find the ideal credit
+          card offer effortlessly with our intuitive comparison tool. Say hello
+          to smart savings!
+        </p>
+        <Button styles="mt-10" />
+      </div>
+      <div className={`${layout.sectionImg}`}>
+        <img src={card} alt="card" className="w-[100%] h-[100%]" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
