3850a24e19ff612061dac779fd7d7565b5f74257
Yunjin <jed.donaire08@gmail.com>
2023-09-24 12:56:28 +0800

    Added Client

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import styles from "../style";
+import { clients } from "../constants";
 
 const Client = () => {
-  return <div>Client</div>;
+  return (
+    <section className={`${styles.flexCenter} `}>
+      <div
+        className={`${styles.flexCenter} flex-wrap w-full ss:flex-row flex-col`}
+      >
+        {clients.map((client) => (
+          <div
+            key={client.id}
+            className={`flex-1 ${styles.flexCenter} ss:min-w-[192px] min-w-[120px]`}
+          >
+            <img
+              src={client.logo}
+              alt="client"
+              className="md:w-[192px] w-[150px] object-contain py-2 md:py-0"
+            />
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Client;
